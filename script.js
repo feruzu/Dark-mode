7f1016fe97dd214a0e0ff4763df9973f0c42abd1
feruzu <fernanda23demarzo@gmail.com>
2022-12-04 22:22:57 -0300

    Botón like

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ function cambiaColor(e) {
         document.documentElement.setAttribute('tema', 'light');
     }
 }
-
 colorDark.addEventListener('change', cambiaColor, false);
+
+
+
+// Botón like
+const boton = document.querySelector('button');
+const corazonLike = document.querySelector('.bx.bxs-heart');
+
+let clicked = false;
+
+boton.addEventListener('click', () => {
+    if(!clicked){
+        clicked = true;
+        document.getElementById("si-like").style.display = "contents"
+        document.getElementById("no-like").style.display = "none"
+    }else{
+        clicked = false;
+        document.getElementById("no-like").style.display = "contents"
+        document.getElementById("si-like").style.display = "none"
+    }
+});
 
